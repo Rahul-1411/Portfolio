a1e3f74ff16ea8baafd372b1e2e63270213b822d
Rahul Thakur <rahulthakur77752@gmail.com>
2025-08-08 23:43:49 +0530

    Second  Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let menu =document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+  var typed = new Typed('.multiple-text', {
+      strings: ['Frontend Developer','Backend Developer','System Designer'],
+      typeSpeed:70,
+      backSpeed:70,
+      backDelay:1000,
+      loop:true,
+    });
